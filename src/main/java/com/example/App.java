b1a6bfbf974f3b4239862000af2ca52a04bde8fd
pavankalyan2717 <pujalap.22.becs@acharya.ac.in>
2025-05-22 12:22:45 +0530

    first commit:pg8

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println("this is the simple world ");
+	int a=5;
+	int b=6;
+	System.out.println("sum of "+a+"and"+b+"is"+sum(a,b));
+    }
+    public static int sum(int x ,int y){
+	    return x+y;
+    }
+ }
